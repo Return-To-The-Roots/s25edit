44745b0421ab572659244e86527bf582253f9dd6
Alexander Grund <alexander.grund@tu-dresden.de>
2019-01-23 10:23:53 +0100

    Fix char comparison in font

--- CIO/CFont.cpp
+++ CIO/CFont.cpp
@@ -70,6 +70,111 @@ void CFont::setMouseData(SDL_MouseButtonEvent button)
     }
 }
 
+unsigned getIndexForChar(uint8_t c)
+{
+    // subtract 32 shows that we start by spacebar as 'zero-position'
+    // subtract another value after subtracting 32 means the skipped chiffres in ansi in compare to our enumeration
+    // (cause we dont have all ansi-values as pictures)
+    if(c >= 32 && c <= 90)
+        return c - 32;
+    /* \ */
+    else if(c == 92)
+        return 59;
+    // _
+    else if(c == 95)
+        return 60;
+    // between 'a' and 'z'
+    else if(c >= 97 && c <= 122)
+        return c - 32 - 4;
+    // ©
+    else if(c == 169)
+        return 114;
+    // Ä
+    else if(c == 196)
+        return 100;
+    // Ç
+    else if(c == 199)
+        return 87;
+    // Ö
+    else if(c == 214)
+        return 106;
+    // Ü
+    else if(c == 220)
+        return 107;
+    // ß
+    else if(c == 223)
+        return 113;
+    // à
+    else if(c == 224)
+        return 92;
+    // á
+    else if(c == 225)
+        return 108;
+    // â
+    else if(c == 226)
+        return 90;
+    // ä
+    else if(c == 228)
+        return 91;
+    // ç
+    else if(c == 231)
+        return 93;
+    // è
+    else if(c == 232)
+        return 96;
+    // é
+    else if(c == 233)
+        return 89;
+    // ê
+    else if(c == 234)
+        return 94;
+    // ë
+    else if(c == 235)
+        return 95;
+    // ì
+    else if(c == 236)
+        return 99;
+    // í
+    else if(c == 237)
+        return 109;
+    // î
+    else if(c == 238)
+        return 98;
+    // ï
+    else if(c == 239)
+        return 97;
+    // ñ
+    else if(c == 241)
+        return 112;
+    // ò
+    else if(c == 242)
+        return 103;
+    // ó
+    else if(c == 243)
+        return 110;
+    // ô
+    else if(c == 244)
+        return 101;
+    // ö
+    else if(c == 246)
+        return 102;
+    // ù
+    else if(c == 249)
+        return 105;
+    // ú
+    else if(c == 250)
+        return 111;
+    // û
+    else if(c == 251)
+        return 104;
+    // ü
+    else if(c == 252)
+        return 88;
+    // chiffre not available, use '_' instead
+    else
+        return 60;
+}
+
 bool CFont::writeText()
 {
     if(string_.empty())
@@ -81,8 +186,6 @@ bool CFont::writeText()
     int row_separator = (fontsize_ == 9 ? 1 : (fontsize_ == 11 ? 3 : 4));
     unsigned int pixel_ctr_h = fontsize_ + row_separator;
     bool pixel_count_loop = true;
-    // the index for the chiffre-picture in the global::bmpArray
-    unsigned int chiffre_index = 0;
     // counter for the drawed pixels (cause we dont want to draw outside of the surface)
     int pos_x = 0;
     int pos_y = 0;
@@ -91,122 +194,17 @@ bool CFont::writeText()
     auto chiffre = string_.begin();
     while(chiffre != string_.end())
     {
+        // the index for the chiffre-picture in the global::bmpArray
+        unsigned int chiffre_index;
         // set chiffre_index to the first chiffre (spacebar) depending on the fontsize
         switch(fontsize_)
         {
             case 9: chiffre_index = FONT9_SPACE; break;
-
-            case 11: chiffre_index = FONT11_SPACE; break;
-
-            case 14: chiffre_index = FONT14_SPACE; break;
-
             default: // in fact not necessary, cause this case is handled by the constructor
-                break;
+            case 11: chiffre_index = FONT11_SPACE; break;
+            case 14: chiffre_index = FONT14_SPACE; break;
         }
-
-        // subtract 32 shows that we start by spacebar as 'zero-position'
-        // subtract another value after subtracting 32 means the skipped chiffres in ansi in compare to our enumeration (cause we dont have
-        // all ansi-values as pictures)
-
-        // between 'spacebar' and the 'Z'
-        if(*chiffre >= 32 && *chiffre <= 90)
-            chiffre_index += (*chiffre - 32) * NUM_FONT_COLORS + color_;
-        /* \ */
-        else if(*chiffre == 92)
-            chiffre_index += 59 * NUM_FONT_COLORS + color_;
-        // _
-        else if(*chiffre == 95)
-            chiffre_index += 60 * NUM_FONT_COLORS + color_;
-        // between 'a' and 'z'
-        else if(*chiffre >= 97 && *chiffre <= 122)
-            chiffre_index += (*chiffre - 32 - 4) * NUM_FONT_COLORS + color_;
-        // ©
-        else if(*chiffre == 169)
-            chiffre_index += 114 * NUM_FONT_COLORS + color_;
-        // Ä
-        else if(*chiffre == 196)
-            chiffre_index += 100 * NUM_FONT_COLORS + color_;
-        // Ç
-        else if(*chiffre == 199)
-            chiffre_index += 87 * NUM_FONT_COLORS + color_;
-        // Ö
-        else if(*chiffre == 214)
-            chiffre_index += 106 * NUM_FONT_COLORS + color_;
-        // Ü
-        else if(*chiffre == 220)
-            chiffre_index += 107 * NUM_FONT_COLORS + color_;
-        // ß
-        else if(*chiffre == 223)
-            chiffre_index += 113 * NUM_FONT_COLORS + color_;
-        // à
-        else if(*chiffre == 224)
-            chiffre_index += 92 * NUM_FONT_COLORS + color_;
-        // á
-        else if(*chiffre == 225)
-            chiffre_index += 108 * NUM_FONT_COLORS + color_;
-        // â
-        else if(*chiffre == 226)
-            chiffre_index += 90 * NUM_FONT_COLORS + color_;
-        // ä
-        else if(*chiffre == 228)
-            chiffre_index += 91 * NUM_FONT_COLORS + color_;
-        // ç
-        else if(*chiffre == 231)
-            chiffre_index += 93 * NUM_FONT_COLORS + color_;
-        // è
-        else if(*chiffre == 232)
-            chiffre_index += 96 * NUM_FONT_COLORS + color_;
-        // é
-        else if(*chiffre == 233)
-            chiffre_index += 89 * NUM_FONT_COLORS + color_;
-        // ê
-        else if(*chiffre == 234)
-            chiffre_index += 94 * NUM_FONT_COLORS + color_;
-        // ë
-        else if(*chiffre == 235)
-            chiffre_index += 95 * NUM_FONT_COLORS + color_;
-        // ì
-        else if(*chiffre == 236)
-            chiffre_index += 99 * NUM_FONT_COLORS + color_;
-        // í
-        else if(*chiffre == 237)
-            chiffre_index += 109 * NUM_FONT_COLORS + color_;
-        // î
-        else if(*chiffre == 238)
-            chiffre_index += 98 * NUM_FONT_COLORS + color_;
-        // ï
-        else if(*chiffre == 239)
-            chiffre_index += 97 * NUM_FONT_COLORS + color_;
-        // ñ
-        else if(*chiffre == 241)
-            chiffre_index += 112 * NUM_FONT_COLORS + color_;
-        // ò
-        else if(*chiffre == 242)
-            chiffre_index += 103 * NUM_FONT_COLORS + color_;
-        // ó
-        else if(*chiffre == 243)
-            chiffre_index += 110 * NUM_FONT_COLORS + color_;
-        // ô
-        else if(*chiffre == 244)
-            chiffre_index += 101 * NUM_FONT_COLORS + color_;
-        // ö
-        else if(*chiffre == 246)
-            chiffre_index += 102 * NUM_FONT_COLORS + color_;
-        // ù
-        else if(*chiffre == 249)
-            chiffre_index += 105 * NUM_FONT_COLORS + color_;
-        // ú
-        else if(*chiffre == 250)
-            chiffre_index += 111 * NUM_FONT_COLORS + color_;
-        // û
-        else if(*chiffre == 251)
-            chiffre_index += 104 * NUM_FONT_COLORS + color_;
-        // ü
-        else if(*chiffre == 252)
-            chiffre_index += 88 * NUM_FONT_COLORS + color_;
-        // chiffre not available, use '_' instead
-        else
-            chiffre_index += 60 * NUM_FONT_COLORS + color_;
+        chiffre_index += getIndexForChar(*chiffre) * NUM_FONT_COLORS + color_;
 
         // if we only count pixels in this round
         if(pixel_count_loop)
@@ -267,7 +265,7 @@ bool CFont::writeText()
         // set position for next chiffre depending on the width of the actual drawn
         // NOTE: there is a bug in the ansi 236 'ì' at fontsize 9, the width is 39, this is not useable, we will use the width of ansi 237
         // 'í' instead
-        if(fontsize_ == 9 && *chiffre == 236)
+        if(fontsize_ == 9 && static_cast<uint8_t>(*chiffre) == 236)
             pos_x += global::bmpArray[FONT9_SPACE + 109 * NUM_FONT_COLORS + color_].w;
         else
             pos_x += global::bmpArray[chiffre_index].w;
